0e81e1224b483d1bbe81858b52dab5283021ceb3
Richie Foreman <richie.foreman@gmail.com>
2012-07-22 02:32:52 -0700

    Revert "Initial Commit"
    This reverts commit d8851f54a627ceee144df73add600aa9a42e9cd4.

--- generate_privatekey.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-openssl pkcs12 -passin pass:notasecret -in privatekey.p12 -nocerts -passout pass:notasecret -out key.pem
-
-# this next line is kind of odd -- but the key is exported with some metadata at the beginning that PyCrypto can't digest.
-tail -n +5 key.pem > privatekey.pem
-rm key.pem
