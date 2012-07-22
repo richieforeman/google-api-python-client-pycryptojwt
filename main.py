d8851f54a627ceee144df73add600aa9a42e9cd4
Richie Foreman <richie.foreman@gmail.com>
2012-07-22 02:31:53 -0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,30 @@
+__author__ = "Richie Foreman <richie.foreman@gmail.com>"
+
+import httplib2
+from apiclient.discovery import build
+
+from PyCryptoSignedJWT import PyCryptoSignedJwtAssertionCredentials
+
+KEY = "privatekey.pem"
+SCOPES = ["https://www.googleapis.com/auth/drive"]
+CLIENT_ID = "89576170682-1v4a6kh1l382akel7fs89an8q6kna61u.apps.googleusercontent.com"
+SERVICE_ACCOUNT = '89576170682-1v4a6kh1l382akel7fs89an8q6kna61u@developer.gserviceaccount.com'
+
+def main():
+    key = open(KEY).read()
+
+    credentials = PyCryptoSignedJwtAssertionCredentials(
+        SERVICE_ACCOUNT,
+        key,
+        scope=" ".join(SCOPES),
+        prn="richard@richieforeman.com")
+    http = httplib2.Http()
+    httplib2.debuglevel = True
+    http = credentials.authorize(http)
+
+    service = build(serviceName='drive', version='v2', http=http)
+
+    print service.files().list().execute()
+
+if __name__ == "__main__":
+    main()
